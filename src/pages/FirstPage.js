04b1b0b75b29d33ec462058749379f44bc6c61d5
chans <pcswer@gmail.com>
2017-11-06 15:24:41 +0900

    [기능] Ant design ui component 추가

--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -1,34 +1,31 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-import * as userActions from 'actions/user'
-import { Link } from 'react-router-dom'
+import * as userActions from "actions/user";
+import { Link } from "react-router-dom";
 // import './FirstPage.css'
+import { Layout } from "antd";
+// import "antd";
 
 class FirstPage extends Component {
-  render() {
-    const b64 = this.props.staticContext ? 'wait for it' : window.btoa('wait for it')
-    return (
-      <div className='bold'>
-        <h2>First Page</h2>
-        <p>{`Email: ${this.props.user.email}`}</p>
-        <p>{`b64: ${b64}`}</p>
-        <Link to={'/second'}>Second</Link>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h2>First Page</h2>
+                <p>{`Email: ${this.props.user.email}`}</p>
+                <Link to={"/second"}>Second</Link>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
-  user: state.user
-})
+    user: state.user
+});
 
 const mapDispatchToProps = dispatch => ({
-  userActions: bindActionCreators(userActions, dispatch)
-})
+    userActions: bindActionCreators(userActions, dispatch)
+});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(FirstPage)
+export default connect(mapStateToProps, mapDispatchToProps)(FirstPage);
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 // import FirstPage from './FirstPage'
 // import SecondPage from './SecondPage'
-import { FirstPage, SecondPage } from 'pages'
-import NoMatch from 'components/NoMatch'
+import { FirstPage, SecondPage } from "pages";
+import NoMatch from "components/NoMatch";
+
+import { Layout } from "antd";
 
 export default class App extends Component {
-  render(){
-    return (
-      <div>
-        <h1>Server Side Rendering with Create React App v2</h1>
-        <Switch>
-          <Route exact path="/" component={FirstPage}/>
-          <Route path="/second" component={SecondPage}/>
-          <Route component={NoMatch}/>
-        </Switch>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                {/* <h1>Server Side Rendering with Create React App v2</h1> */}
+                <Layout>
+                    <Layout.Sider>Hi</Layout.Sider>
+                    <Layout>
+                        <Layout.Header>header</Layout.Header>
+                        <Layout.Content>
+                            <Switch>
+                                <Route exact path="/" component={FirstPage} />
+                                <Route path="/second" component={SecondPage} />
+                                <Route component={NoMatch} />
+                            </Switch>
+                        </Layout.Content>
+                    </Layout>
+                </Layout>
+            </div>
+        );
+    }
 }
